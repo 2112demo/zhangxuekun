01cc40232c9ee7874185ce4da190ce2ca6ecb90b
zxk12 <2216440611@qq.com>
2023-08-23 19:22:43 +0800

    2023-8-23新

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="leftbar">
+      自定义组件 components
+    </div>
+    <div class="rightContent">
+      <router-view/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  background-color: aqua;
+  width: 100vw;
+  height: 100vh;
+}
+.leftbar{
+  background-color: red;
+}
+</style>
---
+++ src/view/index/index.vue
@@ -0,0 +1,30 @@
+<template lang="">
+  <div id='box'>
+    <!-- <div class='box-left'>123</div> -->
+    <div class='box-right'>123</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'index'
+}
+</script>
+<style lang="">
+  *{margin: 0;padding: 0;list-style: none;}
+  #app{
+    margin: 0;padding: 0;
+  }
+  #box{
+    display: flex;
+  }
+  .box-left{
+    width: 15vw;
+    height: 100vh;
+    background-color: #ffffff;
+  }
+  .box-right{
+    width: 85vw;
+    height: 100vh;
+    background-color: #f5f5f5;
+  }
+</style>
